a993d14cb365f084e0ef06c52635109617938b1e
isuzuki <isuzuki@suzukiikuonoAir.lan>
2015-04-26 07:48:08 +0900

    不要なコードを削除

--- app/Http/Controllers/EloquentSampleController.php
+++ app/Http/Controllers/EloquentSampleController.php
@@ -7,8 +7,6 @@ class EloquentSampleController extends Controller {
 
 	public function basic(Artist $artist)
 	{
-		$artist->find(1);
-
 		return view('sample.eloquent.basic', [
 			'artist' => $artist->find(1),
 		]);
